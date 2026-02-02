4d66c7d9514f99193754604c32a2742d8f0c40be
Jarvis Maia <jarvismaia97@gmail.com>
2026-02-02 23:31:12 +0000

    Increase logo size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="flex items-center justify-center px-4 py-3 border-b border-base-300">
-      <img src="/letreco-logo.png" alt="Letreco" className="h-8 md:h-10 select-none" />
+      <img src="/letreco-logo.png" alt="Letreco" className="h-12 md:h-14 select-none" />
     </header>
   );
 }
